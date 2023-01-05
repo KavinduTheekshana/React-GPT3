7cf4208f925b8c74cfdb9c4d9a2bd9ae151e8425
Kavindu Theekshana <kavindutheekshana@gmail.com>
2023-01-05 22:54:18 +0000

    Second

--- src/components/brand/brand.jsx
+++ src/components/brand/brand.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { google, slack, atlassian, dropbox, shopify } from './import';
 import './brand.css'
 
 const brand = () => {
   return (
-    <div>brand</div>
+    <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} />
+    </div>
+    <div>
+      <img src={slack} />
+    </div>
+    <div>
+      <img src={atlassian} />
+    </div>
+    <div>
+      <img src={dropbox} />
+    </div>
+    <div>
+      <img src={shopify} />
+    </div>
+  </div>
   )
 }
 
